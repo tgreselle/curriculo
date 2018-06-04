1738c1fb5fc40c663ac4b9668f028e679751c6d9
THIAGO GRESELLE <tgreselle@hotmail.com>
2018-06-04 19:53:05 -0400

    Adicionar arquivos de projeto.

---
+++ curriculo/Controllers/CurriculoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace curriculo.Controllers
+{
+    public class CurriculoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
